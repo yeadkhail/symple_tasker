254682e4d0c9ed9dcc8f36c7a1eadb0896d292a0
Mohammad Yead Khail <119938455+yeadkhail@users.noreply.github.com>
2023-09-15 22:30:22 +0600

    Created basic structure and added Task header file and implemented some basic fuctions.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //std::cout << "Hello, World!" << std::endl;
+    std::cout<< "MY PROJECT";
     return 0;
 }
