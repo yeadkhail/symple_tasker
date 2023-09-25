9e5f2fd00bdef7a17cfbe66413da88ea02d7068d
Mohammad Yead Khail <119938455+yeadkhail@users.noreply.github.com>
2023-09-25 18:51:37 +0600

    declared initializer class

---
+++ headers/initializer.h
@@ -0,0 +1,16 @@
+//
+// Created by yead on 9/25/23.
+//
+
+#ifndef SYMPLE_TASKER_INITIALIZER_H
+#define SYMPLE_TASKER_INITIALIZER_H
+
+namespace core {
+
+    class initializer {
+
+    };
+
+} // core
+
+#endif //SYMPLE_TASKER_INITIALIZER_H
