0200a95dfbaae166a8fa78d22d59a7ceca64e9e6
Yead <121762721+yead-study@users.noreply.github.com>
2024-01-10 19:02:20 +0600

    Changed the version year

--- functions/sources/core/visuals/Visuals.cpp
+++ functions/sources/core/visuals/Visuals.cpp
@@ -109,7 +109,7 @@ namespace core {
     {
         using namespace std;
         cout<<"sym tasker "<<"(Symple Utilities) "<<"v0.0.1"<<endl;
-        cout<<"Copyright (C) 2023 Team Symple"<<endl;
+        cout<<"Copyright (C) 2024 Team Symple"<<endl;
         cout<<"License GPLv3+: GNU version 3 or later <https://gnu.org/licences/gpl.html>."<<endl<<"This is a free software: You are free to change and redestribute it."<<endl;
         cout<<"There is NO WARRANTY, to the extent permitted by law."<<endl<<endl;
         cout<<"Written by Md Yead Khail Shovo, Zawad Bin Shoukat Tasin , Md Abdul Muqtadir and Faheem Rahman."<<endl;
